39ebd36cb9ece38b9a734a6f7df8d12049dea874
Darya <d.n.morozova1@gmail.com>
2016-08-31 16:10:56 +0300

    форма и анимация

---
+++ js/script.js
@@ -0,0 +1,16 @@
+        var div = document.querySelector(".for_modal_content");
+        var popup = document.querySelector(".modal_content");
+        var storage = localStorage.getItem("[name=person]");
+        var storage_child = localStorage.getItem("[name=children]");
+
+        div.addEventListener("click", function(event) {
+            event.preventDefault();
+            popup.classList.add("modal_content_show");
+            
+            if (storage) {
+            person.value = storage;    
+            }
+            if (storage_child) {
+            children.value = storage;    
+            }
+        });
